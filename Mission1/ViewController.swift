5735dfad149e29fb677d46166bbce25a237653a2
nobo0630 <fantasista_no_22@yahoo.co.jp>
2018-10-26 21:24:20 +0900

    add Mission1

--- Mission1/ViewController.swift
+++ Mission1/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number:Int! = 0
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    
+    @IBAction func AC(_ sender: Any) {
+        number = 0
+        label.text = String(number)
+    }
+    
+    
+    @IBAction func minus(_ sender: Any) {
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    
+    @IBAction func plus(_ sender: Any) {
+        number = number + 1
+        label.text = String(number)
+    }
+    
 }
 
